82481c5d3b03c9508bb16851d193db7bec630125
noureddineios <noureddinelegragui@gmail.com>
2024-03-19 18:42:43 +0000

    Refactor - View image screen

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -1,41 +1,48 @@
-import React from 'react'
-import { Image, StyleSheet, View } from 'react-native'
+import React from "react";
+import { Image, StyleSheet, View } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
-import colors from '../config/colors'
+import colors from "../config/colors";
 
 export default function ViewImageScreen() {
   return (
     <View style={styles.container}>
-         <View style={styles.closeIcon}></View>
-         <View style={styles.deleteIcon}></View>
-    <Image resizeMethod='contain' style={styles.image} source={require("../assets/chair.jpg")}/>
+      <View style={styles.closeIcon}>
+        <MaterialCommunityIcons name="close" color="white" size={35} />
+      </View>
+      <View style={styles.deleteIcon}>
+        <MaterialCommunityIcons
+          name="trash-can-outline"
+          color="white"
+          size={35}
+        />
+      </View>
+      <Image
+        resizeMethod="contain"
+        style={styles.image}
+        source={require("../assets/chair.jpg")}
+      />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "#000",
-        flex: 1
-    },
-    image: {
-        width: "100%",
-        height: "100%",
-    },
-    closeIcon: {
-        width: 50,
-        height: 50,
-        backgroundColor: colors.primary,
-        position:"absolute",
-        top: 40,
-        left: 30
-    },
-    deleteIcon: {
-        width: 50,
-        height: 50,
-        backgroundColor: colors.secondary,
-        position:"absolute",
-        top: 40,
-        right: 30
-    }
-})
+  container: {
+    backgroundColor: "#000",
+    flex: 1,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  closeIcon: {
+    position: "absolute",
+    top: 40,
+    left: 30,
+  },
+  deleteIcon: {
+    position: "absolute",
+    top: 40,
+    right: 30,
+  },
+});
