6d4d9cf7896370f55710927ae78022b77e2f4669
noureddineios <noureddinelegragui@gmail.com>
2024-03-19 22:00:42 +0000

    Create message screen

--- App.js
+++ App.js
@@ -8,7 +8,8 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
-  return <ListingDetailsScreen />;
+  return <MessagesScreen />;
 }
