54f9a0839641394c85bb4071cb335ec83a4cd3e1
noureddineios <noureddinelegragui@gmail.com>
2024-03-19 23:08:39 +0000

    Add List items separator

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { FlatList, StyleSheet } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 
 import ListItem from "../components/ListItem";
 import Screen from "../components/Screen";
+import ListItemSeparator from "../components/ListItemSeparator";
 
 const messages = [
   {
@@ -32,6 +33,7 @@ function MessagesScreen(props) {
             image={item.image}
           />
         )}
+        ItemSeparatorComponent={ListItemSeparator}
       />
     </Screen>
   );
