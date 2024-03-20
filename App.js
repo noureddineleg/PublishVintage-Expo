866b695f5c6feef1ef6e7d73ee222a43abd53a1c
noureddineios <noureddinelegragui@gmail.com>
2024-03-20 03:18:53 +0000

    Create reusable icon component

--- App.js
+++ App.js
@@ -9,7 +9,13 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import MessagesScreen from "./app/screens/MessagesScreen";
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
 
 export default function App() {
-  return <MessagesScreen />;
+  return (
+    <Screen>
+      <Icon name="email" />
+    </Screen>
+  );
 }
