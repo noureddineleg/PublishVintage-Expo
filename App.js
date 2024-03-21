e91579ff070ec3e11fdfffad608810510212ca21
noureddineios <noureddinelegragui@gmail.com>
2024-03-21 03:33:35 +0000

    Create App text input

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, SafeAreaView, TextInput } from "react-native";
 import {
   useDimensions,
   useDeviceOrientation,
@@ -14,7 +14,12 @@ import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
-  return <ListingsScreen />;
+  return (
+    <Screen>
+      <AppTextInput icon={"email"} placeholder="Username" />
+    </Screen>
+  );
 }
