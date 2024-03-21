89d3d47c92d2ed7c07a8e5ae56fbd604fd15e1a1
noureddineios <noureddinelegragui@gmail.com>
2024-03-21 04:10:59 +0000

    Create app picker

--- App.js
+++ App.js
@@ -15,11 +15,13 @@ import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
 import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
 
 export default function App() {
   return (
     <Screen>
-      <AppTextInput icon={"email"} placeholder="Username" />
+      <AppTextInput icon={"email"} placeholder="Email" />
+      <AppPicker icon={"apps"} placeholder="Category" />
     </Screen>
   );
 }
---
+++ app/components/AppPicker.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { View, StyleSheet, Platform } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import colors from "../config/colors";
+import defaultStyles from "../components/AppText/styles";
+import AppText from "./AppText";
+
+function AppPicker({ icon, placeholder, ...otherProps }) {
+  return (
+    <View style={containerStyle.container}>
+      {icon && (
+        <MaterialCommunityIcons
+          name={icon}
+          size={20}
+          color={colors.medium}
+          style={containerStyle.icon}
+        />
+      )}
+      <AppText style={containerStyle.text}>{placeholder}</AppText>
+      <MaterialCommunityIcons
+        name="chevron-down"
+        size={20}
+        color={colors.medium}
+      />
+    </View>
+  );
+}
+
+const containerStyle = StyleSheet.create({
+  container: {
+    backgroundColor: colors.light,
+    borderRadius: 25,
+    flexDirection: "row",
+    width: "100%",
+    padding: 15,
+    marginVertical: 10,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  text: {
+    flex: 1,
+  },
+});
+
+export default AppPicker;
