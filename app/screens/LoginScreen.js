c51914d5845ef0932a493b1e8dbba89872b10b21
noureddineios <noureddinelegragui@gmail.com>
2024-03-27 22:20:34 +0000

    Storing current user

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { StyleSheet, Image } from "react-native";
 import * as Yup from "yup";
+import { jwtDecode } from "jwt-decode";
 
 import Screen from "../components/Screen";
 import {
@@ -10,6 +11,7 @@ import {
   AppForm,
 } from "../components/forms";
 import authApi from "../api/auth";
+import AuthContext from "../auth/context";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
@@ -17,12 +19,15 @@ const validationSchema = Yup.object().shape({
 });
 
 function LoginScreen(props) {
+  const authContext = useContext(AuthContext);
   const [loginFailed, setLoginFailed] = useState(false);
 
   const handleSubmit = async ({ email, password }) => {
     const result = await authApi.login(email, password);
     if (!result.ok) return setLoginFailed(true);
     setLoginFailed(false);
+    const user = jwtDecode(result.data);
+    authContext.setUser(user);
     console.log(result.data);
   };
 
