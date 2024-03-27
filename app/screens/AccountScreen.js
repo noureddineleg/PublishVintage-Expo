0c5eed650201ac4ef906f55a3cd4cef3072467ee
noureddineios <noureddinelegragui@gmail.com>
2024-03-27 22:30:57 +0000

    Handle onPress logout

--- app/screens/AccountScreen.js
+++ app/screens/AccountScreen.js
@@ -27,7 +27,7 @@ const menuItems = [
 ];
 
 function AccountScreen({ navigation }) {
-  const { user } = useContext(AuthContext);
+  const { user, setUser } = useContext(AuthContext);
 
   return (
     <Screen style={styles.screen}>
@@ -60,6 +60,7 @@ function AccountScreen({ navigation }) {
       <ListItem
         title="Log out"
         ImageComponent={<Icon name="logout" backgroundColor="#ffe66d" />}
+        onPress={() => setUser(null)}
       />
     </Screen>
   );
